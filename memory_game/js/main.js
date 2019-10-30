1a41e594c8d6b6568ad1a023e702db1ba3ed0f0f
Allen Joseph <AllenJosephS@JohnDeere.com>
2019-10-30 11:28:33 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,22 @@
-var card1 = 'king';
-var card2 = 'king';
-var card3 = 'queen';
-var card4 = 'queen';
+var cards = ['queen','queen','king','king'];
+var cardsInPlay = [];
+var cardOne;
+var cardTwo;
 
-console.log("User flipped " + card1);
-console.log("User flipped " + card3);
+cardOne = cards[2];
+cardTwo = cards[0];
+
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
+
+for (let i=0; i < cardsInPlay.length; i++){	
+	console.log("User flipped " + cardsInPlay[i]);
+}
+
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		window.alert("You found a match!");
+	} else {
+		window.alert("Sorry, try again.");
+	}
+}
