82a5d4881e22a8a737b10458cdd15441f5a56837
Allen Joseph <AllenJosephS@JohnDeere.com>
2019-10-30 12:50:55 -0500

    created functions

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,22 +1,32 @@
 var cards = ['queen','queen','king','king'];
 var cardsInPlay = [];
-var cardOne;
-var cardTwo;
 
-cardOne = cards[2];
-cardTwo = cards[0];
 
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
-
-for (let i=0; i < cardsInPlay.length; i++){	
-	console.log("User flipped " + cardsInPlay[i]);
+function checkForMatch(){
+		if (cardsInPlay[0] === cardsInPlay[1]) {
+			console.log("You found a match!");
+		} else {
+			console.log("Sorry, try again.");
+		}	
 }
 
-if (cardsInPlay.length === 2) {
-	if (cardsInPlay[0] === cardsInPlay[1]) {
-		window.alert("You found a match!");
-	} else {
-		window.alert("Sorry, try again.");
+function flipCard(cardId) {
+	
+	console.log("User flipped " + cards[cardId]);
+
+	cardsInPlay.push(cards[cardId]);
+
+	for (let i=0; i < cardsInPlay.length; i++){	
+		console.log("All cards in play " + cardsInPlay[i]);
 	}
+
+	if (cardsInPlay.length === 2) {
+		checkForMatch();
+	}
+
 }
+
+flipCard(0);
+flipCard(1);
+
+
