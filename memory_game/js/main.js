b3a76e116390fff1ae128583b969ed58cafb1b9c
Allen Joseph <AllenJosephS@JohnDeere.com>
2019-10-30 14:01:30 -0500

    added objects

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,9 +1,31 @@
-var cards = ['queen','queen','king','king'];
+var cards = [
+	{
+	rank: "queen",
+	suit: "hearts",
+	cardImage: "images/queen-of-hearts.png"
+	},
+	{
+	rank: "queen",
+	suit: "diamonds",
+	cardImage: "images/queen-of-diamonds.png"
+	},
+	{
+	rank: "king",
+	suit: "hearts",
+	cardImage: "images/king-of-hearts"
+	},
+	{
+	rank: "king",
+	suit: "diamonds",
+	cardImage: "images/king-of-diamonds"
+	}
+];
+
 var cardsInPlay = [];
 
 
 function checkForMatch(){
-		if (cardsInPlay[0] === cardsInPlay[1]) {
+		if (cardsInPlay[0].rank === cardsInPlay[1].rank) {
 			console.log("You found a match!");
 		} else {
 			console.log("Sorry, try again.");
@@ -12,12 +34,13 @@ function checkForMatch(){
 
 function flipCard(cardId) {
 	
-	console.log("User flipped " + cards[cardId]);
+	console.log("User flipped " + cards[cardId].rank);
 
 	cardsInPlay.push(cards[cardId]);
 
 	for (let i=0; i < cardsInPlay.length; i++){	
-		console.log("All cards in play " + cardsInPlay[i]);
+		console.log("All cards in play " + cardsInPlay[i].cardImage);
+		console.log("All cards in play " + cardsInPlay[i].suit);
 	}
 
 	if (cardsInPlay.length === 2) {
